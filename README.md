ae560112ebb5d8f36becd6865fb764bc77d79a0e
dev-ol <fureflights@gmail.com>
2021-04-18 20:28:16 -0400

    working on the knapsack problem

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cop4530_knapsack_problem
+
+To compile run :
+
+- g++ -std=c++11 recursion.cpp -o recursion
--- knapsack.cpp
+++ knapsack.cpp
@@ -1,32 +1,44 @@
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int max(int a, int b) { return (a > b) ? a : b; }
 
-int knapSack(int profits[], int weight[], int numItems, int maxWeight);
+int knapSack(double profits[], int weight[], int numItems, int maxWeight);
 
 int main()
 {
+    int numItems = 100;
+    double profits[numItems];
+    int weight[numItems];
+    int maxWeight = 335732;
 
-    int profits[] = {60, 100, 120};
-    int weight[] = {10, 20, 30};
-    int numItems = sizeof(profits) / sizeof(profits[0]);
-    int maxWeight = 50;
+    for(int i=0; i < numItems; i++){
+        weight[i] = int((1000 * pow(i+1,0.5)));
+        profits[i] = ( i+1/(1000 * pow(i+1,0.5)));
+    }
 
-    int result = knapSack(profits, weight, numItems, maxWeight);
 
-    cout << result << endl;
+   int result = knapSack(profits, weight, numItems, maxWeight);
+
+   cout << result << endl;
+
 
     return 0;
 }
 
-int knapSack(int profits[], int weight[], int numItems, int maxWeight)
+int knapSack(double profits[], int weight[], int numItems, int maxWeight)
 {
-    int table[numItems + 1][maxWeight + 1] = {
-        {0, 0},
-        {0, 0}};
+    int x = numItems + 1;
+    int y = maxWeight + 1;
+   
+    int ** table = new int * [x] ;
+
+    for(size_t i = 0; i < numItems+1; i++){
+        table[i] = new int[y];
+    }
 
     for (int i = 0; i < (numItems + 1); i++)
     {
@@ -48,6 +60,12 @@ int knapSack(int profits[], int weight[], int numItems, int maxWeight)
         }
     }
 
+
+    // for(int i=0; i < numItems+1; i++){
+    //     for(int x = 0; x <  maxWeight+1; x++){
+    //         cout << table[i][x] << endl;
+    //     }
+    // }
+    
     return table[numItems][maxWeight];
 }
-8768929679
